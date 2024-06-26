22f639712f939e34888c14179700764ff754c77c
matheusfinamor1 <matheusfinamor1@gmail.com>
2024-06-26 16:27:39 -0300

    implement home screen

--- app/src/main/java/com/example/testemobile/MainActivity.kt
+++ app/src/main/java/com/example/testemobile/MainActivity.kt
@@ -6,17 +6,25 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.example.testemobile.ui.screens.HomeScreen
 import com.example.testemobile.ui.theme.TesteMobileTheme
+import com.example.testemobile.ui.viewmodel.HomeScreenViewModel
+import org.koin.androidx.compose.koinViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContent {
             TesteMobileTheme {
+                val viewModel: HomeScreenViewModel = koinViewModel()
+                val uiState by viewModel.uiState.collectAsStateWithLifecycle()
                 Scaffold(modifier = Modifier.fillMaxSize()) { _ ->
-
+                    HomeScreen(uiState)
                 }
             }
         }
--- app/src/main/java/com/example/testemobile/ui/state/HomeUiState.kt
+++ app/src/main/java/com/example/testemobile/ui/state/HomeUiState.kt
@@ -1,13 +1,7 @@
 package com.example.testemobile.ui.state
 
+import com.example.testemobile.data.Car
+
 data class HomeUiState(
-    val id: Int = 0,
-    val timestampCadastro: Long = 0,
-    val modeloId: String = "",
-    val ano: Int = 0,
-    val combustivel: String = "",
-    val numPortas: Int = 0,
-    val cor: String = "",
-    val nomeModelo: String = "",
-    val valor: Double = 0.0
+    val cars: List<Car> = emptyList()
 )
